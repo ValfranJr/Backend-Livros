2c1b787426b5e33c89c9caafe31a0d160c50a09b
Valfran <valfran-goncalves@hotmail.com>
2025-07-21 14:01:27 -0300

    feat: adicionando estruturação dentro do BackEnd

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # API de livros
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 # Dicionário para armazenar os livros cadastrados
 meus_livros = {}
+
+
+class Livro(BaseModel):
+    nome: str
+    autor: str
+    ano: int
+
 
 # Rota para listar os livros cadastrados
 @app.get("/livros")
@@ -13,32 +22,27 @@ def get_livros():
     else:
         return {"livros": meus_livros}
 
+
 # Rota para cadastrar um livro
 @app.post("/adiciona")
-def post_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+def post_livros(id_livro: int, livro: Livro):
     if id_livro in meus_livros:
         raise HTTPException(status_code=400, detail="Livro já cadastrado.")
     else:
-        meus_livros[id_livro] = {
-            "nome": nome_livro,
-            "autor": autor_livro,
-            "ano": ano_livro
-        }
+        meus_livros[id_livro] = livro.model_dump()
         return {"message": "Livro cadastrado com sucesso."}
+
+
 # Rota para atualizar um livro
 @app.put("/atualiza/{id_livro}")
-def put_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+def put_livros(id_livro: int, livro: Livro):
     meu_livro = meus_livros.get(id_livro)
     if not meu_livro:
         raise HTTPException(status_code=404, detail="Livro não encontrado.")
     else:
-        if nome_livro:
-            meu_livro["nome"] = nome_livro
-        if autor_livro:
-            meu_livro["autor"] = autor_livro
-        if ano_livro:
-            meu_livro["ano"] = ano_livro
+        meu_livro[id_livro] = livro.model_dump()
         return {"message": "Livro atualizado com sucesso."}
+
 
 # Rota para excluir um livro
 @app.delete("/deletar/{id_livro}")
@@ -48,4 +52,3 @@ def delete_livro(id_livro: int):
     else:
         del meus_livros[id_livro]
         return {"message": "Livro excluído com sucesso."}
-
